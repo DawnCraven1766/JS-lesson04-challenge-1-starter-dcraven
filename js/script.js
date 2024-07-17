b57ab8d2f20873ec81c0437ed6c8c0bb1979d547
Dawn Craven <cravenew@aol.com>
2024-07-17 11:03:06 -0700

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+/**console.log("Hi!");**/
+var mainTitle = querySelector("h1");
+console.log(mainTitle.outerHTML);
+
+mainTitle.innerText = "Yay! Javascript";
+
+var body = document.querySelector("body");
+
+console.log(body);
+
+body.style.backgroundColor = #fef2f1;
